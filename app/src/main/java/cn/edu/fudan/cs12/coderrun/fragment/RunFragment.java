cc813739f0e6397f86c0d1182c426fa967032bcc
Yanye Li <yyli12@fudan.edu.cn>
2015-11-30 18:18:48 +0800

    add gps velocity meter

--- app/src/main/java/cn/edu/fudan/cs12/coderrun/fragment/RunFragment.java
+++ app/src/main/java/cn/edu/fudan/cs12/coderrun/fragment/RunFragment.java
@@ -1,11 +1,18 @@
 package cn.edu.fudan.cs12.coderrun.fragment;
 
+import android.Manifest;
 import android.animation.Animator;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Animatable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,27 +30,53 @@ import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import cn.edu.fudan.cs12.coderrun.Config;
 import cn.edu.fudan.cs12.coderrun.R;
 import cn.edu.fudan.cs12.coderrun.action.RunAction;
 import cn.edu.fudan.cs12.coderrun.action.UserAction;
 import cn.edu.fudan.cs12.coderrun.entity.User;
 import cn.edu.fudan.cs12.coderrun.event.ProfileEvent;
+import cn.edu.fudan.cs12.coderrun.event.RunEvent;
 import cn.edu.fudan.cs12.coderrun.provider.BusProvider;
 import de.halfbit.tinybus.Subscribe;
 import mehdi.sakout.fancybuttons.FancyButton;
 
 
 public class RunFragment extends Fragment {
+	enum State {STOP, PAUSE, RUNNING};
 	User user;
 	ImageView mRunImage;
 	FancyButton mRunButton;
 	FancyButton mPauseButton;
+	FancyButton mStopButton;
+	FancyButton mResumeButton;
+	View mDataDisplay;
+	Chronometer ch;  //计时器
+	TextView distanceText;//记录距离的文本框
+	TextView speedText;//记录速度的文本框
+	LocationManager locationManager;
+	PackageManager packageManager;
+	Location initialLocation;
+	Location lastLocation;
+	List<Location> locationList;
+	double totalDist;
+	double avgSpeed;
+	long timeFix;
+	State state;
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		user = UserAction.getCurrentUser();
+		state = State.STOP;
+		timeFix = 0;
+		locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+		packageManager = getActivity().getPackageManager();
 	}
 
 	@Override
@@ -67,33 +100,154 @@ public class RunFragment extends Fragment {
 
 		mRunButton = (FancyButton) v.findViewById(R.id.button_run);
 		mPauseButton = (FancyButton) v.findViewById(R.id.button_pause);
+		mStopButton = (FancyButton) v.findViewById(R.id.button_stop);
+		mResumeButton = (FancyButton) v.findViewById(R.id.button_resume);
 		mRunImage = (ImageView) v.findViewById(R.id.image_run);
+		mDataDisplay = v.findViewById(R.id.set_data);
+		displayCorrespondingView();
+
+
+		//jiao adds on 2015/11/29
+		ch = (Chronometer) v.findViewById(R.id.chronometer1);
+		ch.setFormat("时长：%s");
+		distanceText=(TextView)v.findViewById(R.id.distance_value);
+		speedText=(TextView)v.findViewById(R.id.speed_value);
 
 		mRunButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				ch.setBase(SystemClock.elapsedRealtime());
+				ch.setFormat("时长：%s");
+				ch.start();
+				timeFix = 0;
+				state = State.RUNNING;
+				displayCorrespondingView();
+
 				Animation anim = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_right_out);
 				anim.setAnimationListener(new Animation.AnimationListener() {
 					@Override
-					public void onAnimationStart(Animation animation) {}
+					public void onAnimationStart(Animation animation) {
+					}
+
 					@Override
-					public void onAnimationRepeat(Animation animation) {}
+					public void onAnimationRepeat(Animation animation) {
+					}
+
 					@Override
 					public void onAnimationEnd(Animation animation) {
-						mRunImage.setVisibility(View.GONE);
+						mRunImage.setVisibility(View.INVISIBLE);
 						Toast.makeText(getActivity(), "run", Toast.LENGTH_SHORT).show();
+						startRun();
 					}
 				});
 				mRunImage.startAnimation(anim);
 			}
 		});
+
+		mResumeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ch.setBase(SystemClock.elapsedRealtime() - timeFix);
+				ch.start();
+				state = State.RUNNING;
+				displayCorrespondingView();
+				Toast.makeText(getActivity(), "resume", Toast.LENGTH_SHORT).show();
+
+			}
+		});
 		mPauseButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				timeFix = SystemClock.elapsedRealtime() - ch.getBase();
+				ch.stop();
+				state = State.PAUSE;
+				displayCorrespondingView();
 				Toast.makeText(getActivity(), "pause", Toast.LENGTH_SHORT).show();
+
+			}
+		});
+		mStopButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				ch.stop();
+				state = State.STOP;
+				displayCorrespondingView();
+				Toast.makeText(getActivity(), "stop", Toast.LENGTH_SHORT).show();
 			}
 		});
 		return v;
+	}
+
+	private void startRun() {
+		if (packageManager.checkPermission(Manifest.permission.ACCESS_FINE_LOCATION, getActivity().getPackageName()) == PackageManager.PERMISSION_GRANTED) {
+			StringBuilder recordLocation = new StringBuilder();
+			lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			totalDist = 0.0;
+			locationList = new ArrayList<>();
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+				@Override
+				public void onLocationChanged(Location location) {
+					if (state == State.RUNNING) {
+						Location presentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+						float distance[] = new float[1];
+						Location.distanceBetween(presentLocation.getLatitude(), presentLocation.getLongitude(), lastLocation.getLatitude(), lastLocation.getLongitude(), distance);
+						totalDist += distance[0];
+						double totalTime = (SystemClock.elapsedRealtime() - ch.getBase()) / 1000; //使单位为秒
+						avgSpeed = (totalTime / 60) / (totalDist / 1000);
+						speedText.setText(String.format("%.2f min/km", avgSpeed));
+						distanceText.setText(String.format("%.2f km", totalDist / 1000));
+						locationList.add(lastLocation);
+						lastLocation = presentLocation;
+					}
+				}
+
+				@Override
+				public void onStatusChanged(String provider, int status, Bundle extras) {
+
+				}
+
+				@Override
+				public void onProviderEnabled(String provider) {
+
+				}
+
+				@Override
+				public void onProviderDisabled(String provider) {
+
+				}
+			});
+		} else {
+			Toast.makeText(getActivity(), "没有获取地理位置的权限", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private void displayCorrespondingView() {
+		switch (state) {
+			case STOP:
+				mRunButton.setVisibility(View.VISIBLE);
+				mResumeButton.setVisibility(View.INVISIBLE);
+				mPauseButton.setVisibility(View.INVISIBLE);
+				mStopButton.setVisibility(View.INVISIBLE);
+				mDataDisplay.setVisibility(View.INVISIBLE);
+				mRunImage.setVisibility(View.VISIBLE);
+				break;
+			case RUNNING:
+				mRunButton.setVisibility(View.INVISIBLE);
+				mResumeButton.setVisibility(View.INVISIBLE);
+				mPauseButton.setVisibility(View.VISIBLE);
+				mStopButton.setVisibility(View.VISIBLE);
+				mDataDisplay.setVisibility(View.VISIBLE);
+				mRunImage.setVisibility(View.INVISIBLE);
+				break;
+			case PAUSE:
+				mRunButton.setVisibility(View.INVISIBLE);
+				mResumeButton.setVisibility(View.VISIBLE);
+				mPauseButton.setVisibility(View.INVISIBLE);
+				mStopButton.setVisibility(View.VISIBLE);
+				mDataDisplay.setVisibility(View.VISIBLE);
+				mRunImage.setVisibility(View.INVISIBLE);
+				break;
+		}
 	}
 
 }
